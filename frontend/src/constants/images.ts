aa57afe0dcbf460e9bb94880a86c2c7753c58adf
anadreth <miroslav13vano@gmail.com>
2023-02-09 22:13:45 +0100

    ...

--- frontend/src/constants/images.ts
+++ frontend/src/constants/images.ts
@@ -1,9 +1,5 @@
-import bigBlob from './../assets/bigBlob.png'
-import boySitting from './../assets/boySitting.png'
+
 import boyStanding from './../assets/boyStanding.png'
-import purpleBlob from './../assets/purpleBlob.png'
-import smallBlob from './../assets/smallBlob.png'
-import combo from './../assets/combo.png'
 import css from './../assets/css.png'
 import html from './../assets/html.png'
 import javascript from './../assets/javascript.png'
@@ -14,21 +10,18 @@ import node from './../assets/node.png'
 import redux from './../assets/redux.png'
 import figma from './../assets/figma.png'
 import git from './../assets/git.png'
-import dashboard from './../assets/dashboard.png'
-import home from './../assets/home.png'
-import race from './../assets/creatorollRace.png'
 import brand from './../assets/brand-black.png'
 import logo from './../assets/logo-clear.png'
 import mdb from './../assets/mdb.png'
 import ps from './../assets/ps.png'
 import tablet from './../assets/tablet.png'
-import laptop from './../assets/laptop.png'
 import linked from './../assets/logos/linked.svg'
 import github from './../assets/logos/github.svg'
 import cv from  './../assets/miroslav_vano_cv.pdf'
 import tailwind from './../assets/tailwind.png'
 
 export default {
+    boyStanding,
     git,
     figma,
     redux,
